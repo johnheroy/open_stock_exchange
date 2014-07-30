cc0a36a99d7e09c85f169d31b1833bbd9de7fb55
John Heroy <johnheroy@gmail.com>
2014-07-31 00:56:33 +0800

    Display new orders

--- index.js
+++ index.js
@@ -17,12 +17,112 @@ app.get('/latency-test', function(req, res) {
   res.send((Math.random() * 100).toString());
 });
 
+// arrays for buy / sell orders, two-dimensional
+var buyOrders = new Array();
+var sellOrders = new Array();
+var lastTrade; // update to last trade
+
+// sockets
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
-  })
+  });
+
+  socket.on('new order', function(order){
+    var timeReceived = new Date().getTime();
+    console.log('new order received');
+    console.log(order);
+
+    // delay adding order inversely by the latency
+    setTimeout(function(){
+      addOrder(order, timeReceived);
+      io.emit('new order book', {
+        buys: buyOrders,
+        sells: sellOrders
+      });
+      // matchOrders();
+    }, (500 - order.latency));    
+  });
 });
+
+var matchOrders = function(){
+  // start matching orders in priority
+};
+
+var addOrder = function(order, timeReceived){
+  var timePriority = timeReceived - order.latency;
+  var orderObj = {
+    shares: order.shares,
+    limit: order.limit,
+    time: timePriority
+  };
+  switch (order.type) {
+    case 'buy':
+      console.log('entering buy order');
+      if (buyOrders.length === 0){
+        buyOrders.push(orderObj);
+        console.log(buyOrders);
+      } else {
+        var orderEntered = false;
+        for (var i = 0; i < buyOrders.length; i++){
+          if (order.limit > buyOrders[i].limit){
+            buyOrders.splice(i, 0, orderObj);
+            orderEntered = true;
+            console.log(buyOrders);
+            break;
+          } else if (order.limit === buyOrders[i].limit){
+            if (timePriority < buyOrders[i].time){
+              buyOrders.splice(i, 0, orderObj);
+              orderEntered = true;
+              console.log(buyOrders);
+              break;
+            } // else move on
+            //break;
+          } else if (order.limit < buyOrders[i].limit){
+            // move on
+          }
+        }
+        if (!orderEntered) {
+          buyOrders.push(orderObj); 
+          console.log(buyOrders);  
+        }
+      }
+      break;
+
+    case 'sell':
+      console.log('entering sell order');
+      if (sellOrders.length === 0){
+        sellOrders.push(orderObj);
+        console.log(sellOrders);
+      } else {
+        var orderEntered = false;
+        for (var i = 0; i < sellOrders.length; i++){
+          if (order.limit < sellOrders[i].limit){
+            sellOrders.splice(i, 0, orderObj);
+            orderEntered = true;
+            console.log(sellOrders);
+            break;
+          } else if (order.limit === sellOrders[i].limit){
+            if (timePriority < sellOrders[i].time){
+              sellOrders.splice(i, 0, orderObj);
+              orderEntered = true;
+              console.log(sellOrders);
+              break;
+            } // else move on
+            //break;
+          } else if (order.limit > sellOrders[i].limit){
+            // move on
+          }
+        }
+        if (!orderEntered) {
+          sellOrders.push(orderObj); 
+          console.log(sellOrders);  
+        }
+      }
+      break;
+  }
+};
 
 // start the server
 var port = Number(process.env.PORT || 5000);
--- public/javascripts/welcome.js
+++ public/javascripts/welcome.js
@@ -1,8 +1,53 @@
 $(document).ready(function(){ 
   var socket = io();
 
+  $('.buy-button').on('click', function(){
+    submitNewOrder('buy');
+  });
+
+  $('.sell-button').on('click', function(){
+    submitNewOrder('sell');
+  });
+
+  var submitNewOrder = function(type){
+    socket.emit('new order', {
+      shares: $('.shares-entry').val(),
+      limit: $('.limit-entry').val(),
+      type: type,
+      latency: currentAverageLatency
+    });
+  };
+
+  socket.on('new order book', function(book){
+    console.log(book);
+    var buyOrders = book.buys;
+    var sellOrders = book.sells;
+
+    var maxLengthBuys;
+    if (buyOrders.length < 10){
+      maxLengthBuys = buyOrders.length;
+    } else {
+      maxLengthBuys = 10;
+    }
+    for (var i = 1; i <= maxLengthBuys; i++){
+      var orderText = buyOrders[i-1].shares + ' shares @ limit ' + buyOrders[i-1].limit;
+      $('.buy-' + i).text(orderText);
+    }
+
+    var maxLengthSells;
+    if (sellOrders.length < 10){
+      maxLengthSells = sellOrders.length;
+    } else {
+      maxLengthSells = 10;
+    }
+    for (var i = 1; i <= maxLengthSells; i++){
+      var orderText = sellOrders[i-1].shares + ' shares @ limit ' + sellOrders[i-1].limit;
+      $('.sell-' + i).text(orderText);
+    }    
+  });
 
   var last5latencies = new Array();
+  var currentAverageLatency;
   // is this going to leak memory?
   setInterval(function(){
     var start = new Date().getTime();
@@ -12,8 +57,8 @@ $(document).ready(function(){
     }).done(function(msg){
       var end = new Date().getTime();
       var latency = (end - start) / 2;
-      console.log('got response ' + msg);
-      console.log('last latency is: ' + latency);
+      // console.log('got response ' + msg);
+      // console.log('last latency is: ' + latency);
       last5latencies.push(latency);
       if (last5latencies.length > 5) {
         last5latencies.shift();
@@ -23,9 +68,10 @@ $(document).ready(function(){
         averageLatency += (last5latencies[i] / last5latencies.length);
       }
       averageLatency = Math.round(averageLatency * 100) / 100;
+      currentAverageLatency = averageLatency;
       $('.latency').text(averageLatency + ' ms');
-      console.log('last 5 latencies are: ' + last5latencies);
-      console.log('average latency is: ' + averageLatency);
+      // console.log('last 5 latencies are: ' + last5latencies);
+      // console.log('average latency is: ' + averageLatency);
     });
   }, 1000);
 
