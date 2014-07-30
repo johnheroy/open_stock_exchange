57cdacded5e6639fe123631eef404612fbf0e535
John Heroy <johnheroy@gmail.com>
2014-07-30 18:19:33 +0800

    Fix connection to socket io

--- index.js
+++ index.js
@@ -19,10 +19,13 @@ app.get('/latency-test', function(req, res) {
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  })
 });
 
 // start the server
 var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
+http.listen(port, function() {
   console.log('Listening on ' + port);
 });
