04ca58606f7f8ffc1e11432748876e9b30fb04cb
John Heroy <johnheroy@gmail.com>
2014-07-30 14:53:53 +0800

    Check latency every 5 seconds

--- public/javascripts/welcome.js
+++ public/javascripts/welcome.js
@@ -5,9 +5,9 @@ setInterval(function(){
     type: 'GET',
     url: '/latency-test'
   }).done(function(msg){
-    console.log('got response');
+    console.log('got response ' + msg);
     var end = new Date().getTime();
     console.log('latency is: ' + ((end - start) / 2) + ' ms');
     $('.latency').text(((end - start) / 2) + ' ms');
   });
-}, 1000);
+}, 5000);
